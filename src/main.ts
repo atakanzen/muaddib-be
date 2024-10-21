4ed7f88b2a729ef8eace89a94bf56f0924b5ddb2
limtis0 <limtis2109@gmail.com>
2024-10-21 15:14:49 +0200

    CORS and compression

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { ValidationPipe } from '@nestjs/common';
+import compression from '@fastify/compress';
 
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter()
-  );
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+    const app = await NestFactory.create<NestFastifyApplication>(
+        AppModule,
+        new FastifyAdapter(),
+        {
+            cors: true
+        }
+    );
+
+    app.register(compression, { encodings: ['gzip', 'deflate', 'br'] });
+
+    app.useGlobalPipes(new ValidationPipe());
+    await app.listen(3000);
 }
 bootstrap();
