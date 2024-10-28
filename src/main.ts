9f2e5f9662c3afb2adcc724bc10d1603f104a002
limtis0 <limtis2109@gmail.com>
2024-10-28 01:18:31 +0100

    Swagger and lib update

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 
 async function bootstrap() {
@@ -12,6 +13,14 @@ async function bootstrap() {
             cors: true
         }
     );
+
+    // Swagger setup
+    const document = new DocumentBuilder()
+        .setTitle('API Overview')
+        .addBearerAuth()
+        .build();
+
+    SwaggerModule.setup('swagger', app, () => SwaggerModule.createDocument(app, document));
 
     // Validation
     app.useGlobalPipes(new ValidationPipe());
